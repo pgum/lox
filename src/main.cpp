fc91005bcd24d059d45ca7dc0159e62c424f2ed2
pgum <killmepl@gmail.com>
2019-05-26 23:24:54 +0200

    proper main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 #include <sysexits.h>
 #include <iostream>
-
-using namespace std;
-
-int main(){
-cout<<"Siema!\n";
+#include <string>
+#include "lox.hpp"
+void printHeader(std::string content){
+  std::cout<< "--= CLox++ =--" << std::endl << content << std::endl;
+  }
+int main(int argc, char *argv[]){
+  Lox::Lox lox;
+  if(argc > 2){
+    printHeader("Usage: " + std::string(argv[0]) + " [script]");
+    return EX_USAGE;
+  }else if(argc == 2){
+    std::string filename(argv[1]);
+    printHeader("Running file: " + filename);
+    lox.runFile(filename);
+  }else {
+    printHeader("REPL");
+    lox.runPrompt();
+  }
 return EX_OK;
 }
