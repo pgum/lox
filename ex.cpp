594a81a96eb518ae3ec22b98f1a7c528c05f6c9c
killme <killme@pop-os.localdomain>
2019-06-18 09:08:40 +0200

    working example refined

--- ex.cpp
+++ ex.cpp
@@ -8,50 +8,45 @@ using Token= int;
 using Error= std::string;
 using Tokens= std::vector<Token>;
 using Errors= std::vector<Error>;
-using ScannerOutput= std::tuple<Tokens,Errors,bool>;
-
-class Scanner{
-  public:
-  Scanner(): isTokenized(false) {}
-
-  ScannerOutput scan(std::string command){
-    *this = Scanner();
-    rawsource = command;
-    source= rawsource;
-    return someHardcoreScanning();
-  }
-
-  private:
-  std::string rawsource, source;
+//using ScannerOutput= std::tuple<Tokens,Errors>;
+struct ScannerOutput {
   Tokens tokens;
   Errors errors;
-  bool isTokenized;
-  const static ScannerOutput EmptySourceOutput;
-  ScannerOutput someHardcoreScanning(){
-    Tokens tt = Tokens(10); for(const auto& t: tt){ tokens.emplace_back(t);}
-    for(const auto& e: {std::string("radio"),std::string("rmf"),std::string("fm")}){errors.emplace_back(e);}
-    isTokenized=true;
-    return {tokens, errors, isTokenized};
+  friend std::ostream & operator << (std::ostream &out, const ScannerOutput &s){
+    out << "### S\n";
+    out << s.tokens.size() << " Tokens: ";
+    for(auto const& t: s.tokens) out << t << " ";
+    out << '\n' << s.errors.size() << " Errors: ";
+    for(auto const& e: s.errors) out << e << " ";
+    return out << std::endl;
   }
 };
 
-const ScannerOutput Scanner::EmptySourceOutput = ScannerOutput({42},{"meaning?"},false);
+class Scanner{
+  public:
+  Scanner() {}
 
-void describe(const ScannerOutput& s){
-  std::cout << std::get<0>(s).size() << "Tokens: ";
-  for(auto const& t: std::get<0>(s)) std::cout << t << " ";
-  std::cout << std::endl;
-  std::cout << std::get<1>(s).size() << "Errors: ";
-  for(auto const& e: std::get<1>(s)) std::cout << e << " ";
-  std::cout << std::endl << "isTokenized: " << std::get<2>(s) << std::endl;
-}
+  ScannerOutput scan(std::string command){
+    if(command == "") return {{42},{"meaning?"}};
+    std::string rawsource, source;
+    Tokens tokens;
+    Errors errors;
+    rawsource = command;
+    source= rawsource;
+
+    for(Tokens tt = Tokens(10); const auto& t: tt){ tokens.emplace_back(t);}
+
+    for(const auto& e: {std::string("radio"),std::string("rmf"),std::string("fm")}){errors.emplace_back(e);}
+    return {tokens, errors};
+  }
+};
+
 
 int main(){
   Scanner s;
-  std::cout << "### S" << std::endl;
-  describe(s.scan(""));
-  auto so=s.scan("");
-  describe(so);
+  std::cout << s.scan("");
+  auto so=s.scan(".");
+  std::cout << so;
   auto s2= s.scan("][");
-  describe(s2);
+  std::cout << s2;
 }
