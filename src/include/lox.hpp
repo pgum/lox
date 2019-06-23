e9ccf7fb4f5cfe942d65275fa3863345e6d914d4
pgum <killmepl@gmail.com>
2019-06-23 20:06:07 +0200

    minor: remove comment, move lines

--- src/include/lox.hpp
+++ src/include/lox.hpp
@@ -8,17 +8,18 @@
 #include "error.hpp"
 #include "token.hpp"
 #include "scanner.hpp"
+
 namespace Lox {
 class Scanner;
 
 class Lox {
-  private:
-  bool hadError;
-  //void error(const Error& error);
   public:
   std::vector<Error> errors;
   std::string run(std::string source);
   void runFile(std::string filename);
   void runPrompt();
+
+  private:
+  bool hadError;
 };
 }
