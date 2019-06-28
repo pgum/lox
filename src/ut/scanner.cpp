d39508beb7dd4bbdb7f5278ad631bcfaca568fb2
pgum <killmepl@gmail.com>
2019-06-28 23:53:45 +0200

    name changing for better ut output

--- src/ut/scanner.cpp
+++ src/ut/scanner.cpp
@@ -14,195 +14,195 @@ TEST_CASE("Erroreus", "[Scanner][Edgecase][Error]"){
   Lox::Input invalid_command = "$";
   auto out = sut.scan(invalid_command);
   REQUIRE(out.errors.empty() == false);
-  Lox::Lexems lexems ={ "\0" };
-  REQUIRE(out.lexems == lexems);
+  Lox::Lexems expected ={ "\0" };
+  REQUIRE(out.lexems == expected);
 }
 
 TEST_CASE("Empty input", "[Scanner][Edgecase][Empty]"){
   Lox::Input command = "";
   auto out = sut.scan(command);
   CheckNoErrors(out);
-  Lox::Lexems lexems ={ "\0" };
-  REQUIRE(out.lexems == lexems);
+  Lox::Lexems expected ={ "\0" };
+  REQUIRE(out.lexems == expected);
 }
 
 TEST_CASE("Single and double character operators", "[Scanner][Single][Operator]"){
-  Lox::Lexems lexems = {"(", ")", "{", "}", "[", "]", ",", ".", ";", ":", "/", "*",
+  Lox::Lexems commands = {"(", ")", "{", "}", "[", "]", ",", ".", ";", ":", "/", "*",
                    "!", "=", ">", "<", "==", "!=", ">=", "<=", "->", "-", "+"};
-  for(auto const& t: lexems){
-    auto out = sut.scan(t);
+  for(auto const& c: commands){
+    auto out = sut.scan(c);
     CheckNoErrors(out);
-    Lox::Lexems lexems ={ t, "\0" };
-    REQUIRE(out.lexems == lexems);
+    Lox::Lexems expected ={ c, "\0" };
+    REQUIRE(out.lexems == expected);
   }
 }
 
-TEST_CASE("raw comments //", "[Scanner][Long][Comments]"){
+TEST_CASE("raw comments //", "[Scanner][Comments]"){
   Lox::Input command = "//This is pretty nice comment!";
   auto out = sut.scan(command);
   CheckNoErrors(out);
-  Lox::Lexems lexems ={ "//This is pretty nice comment!", "\0" };
-  REQUIRE(out.lexems == lexems);
+  Lox::Lexems expected ={ "//This is pretty nice comment!", "\0" };
+  REQUIRE(out.lexems == expected);
 }
 
-TEST_CASE("//short comment", "[Scanner][Short][Comments]"){
-  Lox::Input command = "//helo!";
+TEST_CASE("whitespaces", "[Scanner][Whitespaces]"){
+  Lox::Input command = "1.2 22\t33   44\n\n55 \t \t \t helo";
   auto out = sut.scan(command);
   CheckNoErrors(out);
-  Lox::Lexems lexems ={ "//helo!", "\0" };
-  REQUIRE(out.lexems == lexems);
+  Lox::Lexems expected ={ "1.2", "22", "33", "44", "55", "helo", "\0" };
+  REQUIRE(out.lexems == expected);
 }
 
-TEST_CASE("comments and other lexems", "[Scanner][Long][Comments]"){
-  Lox::Input command = "(){}//This is pretty nice comment!";
+TEST_CASE("//short comment", "[Scanner][Comments]"){
+  Lox::Input command = "//helo!\n//comment";
   auto out = sut.scan(command);
   CheckNoErrors(out);
-  Lox::Lexems lexems = {"(", ")", "{", "}", "//This is pretty nice comment!","\0" };
-  REQUIRE(out.lexems == lexems);
+  Lox::Lexems expected ={ "//helo!", "//comment", "\0" };
+  REQUIRE(out.lexems == expected);
 }
 
-TEST_CASE("numbers - positive integer 1.2.", "[Scanner][Numbers][Integer][Positive][adbd]"){
+TEST_CASE("numbers - positive integer 1.2.", "[Scanner][Numbers][Integer][Positive]"){
   Lox::Input command = "1.2.";
   auto out = sut.scan(command);
   CheckNoErrors(out);
-  Lox::Lexems lexems ={ "1.2", "." , "\0" };
-  REQUIRE(out.lexems == lexems);
+  Lox::Lexems expected ={ "1.2", "." , "\0" };
+  REQUIRE(out.lexems == expected);
 }
 
-TEST_CASE("numbers - positive integer 1.2.3", "[Scanner][Numbers][Integer][Positive][adbdc]"){
+TEST_CASE("numbers - positive integer 1.2.3", "[Scanner][Numbers][Integer][Positive]"){
   Lox::Input command = "1.2.3";
   auto out = sut.scan(command);
   CheckNoErrors(out);
-  Lox::Lexems lexems ={ "1.2", ".", "3", "\0" };
-  REQUIRE(out.lexems == lexems);
+  Lox::Lexems expected ={ "1.2", ".", "3", "\0" };
+  REQUIRE(out.lexems == expected);
 }
 
-TEST_CASE("numbers - positive integer 12", "[Scanner][Numbers][Integer][Positive][ab]"){
+TEST_CASE("numbers - positive integer 12", "[Scanner][Numbers][Integer][Positive]"){
   Lox::Input command = "12";
   auto out = sut.scan(command);
   CheckNoErrors(out);
-  Lox::Lexems lexems ={ "12", "\0" };
-  REQUIRE(out.lexems == lexems);
+  Lox::Lexems expected ={ "12", "\0" };
+  REQUIRE(out.lexems == expected);
 }
 
-TEST_CASE("numbers - positive integer", "[Scanner][Numbers][Integer][Positive][abc]"){
+TEST_CASE("numbers - positive integer", "[Scanner][Numbers][Integer][Positive]"){
   Lox::Input command = "123";
   auto out = sut.scan(command);
   CheckNoErrors(out);
-  Lox::Lexems lexems ={ "123", "\0" };
-  REQUIRE(out.lexems == lexems);
+  Lox::Lexems expected ={ "123", "\0" };
+  REQUIRE(out.lexems == expected);
 }
 
-TEST_CASE("numbers - negative integer", "[Scanner][Numbers][Integer][Negative][mabc]"){
+TEST_CASE("numbers - negative integer", "[Scanner][Numbers][Integer][Negative]"){
   Lox::Input command = "-123";
   auto out = sut.scan(command);
   CheckNoErrors(out);
-  Lox::Lexems lexems ={ "-123", "\0" };
-  REQUIRE(out.lexems == lexems);
+  Lox::Lexems expected ={ "-123", "\0" };
+  REQUIRE(out.lexems == expected);
 }
 
-TEST_CASE("numbers - positive floats", "[Scanner][Long][Numbers][Float][Positive][abdc]"){
+TEST_CASE("numbers - positive floats", "[Scanner][Numbers][Float][Positive]"){
   Lox::Input command = "12.3";
   auto out = sut.scan(command);
   CheckNoErrors(out);
-  Lox::Lexems lexems ={"12.3","\0" };
-  REQUIRE(out.lexems == lexems);
+  Lox::Lexems expected ={"12.3","\0" };
+  REQUIRE(out.lexems == expected);
 }
 
-TEST_CASE("numbers - negative floats", "[Scanner][Long][Numbers][Float][Negative][mabdc]"){
+TEST_CASE("numbers - negative floats", "[Scanner][Numbers][Float][Negative]"){
   Lox::Input command = "-12.3";
   auto out = sut.scan(command);
   CheckNoErrors(out);
-  Lox::Lexems lexems ={"-12.3","\0" };
-  REQUIRE(out.lexems == lexems);
+  Lox::Lexems expected ={"-12.3","\0" };
+  REQUIRE(out.lexems == expected);
 }
 
-TEST_CASE("numbers - not a number: -.", "[Scanner][Numbers][Not][md]"){
+TEST_CASE("numbers - not a number: -.", "[Scanner][Operator][Numbers]"){
   Lox::Input command = "-.";
   auto out = sut.scan(command);
   CheckNoErrors(out);
-  Lox::Lexems lexems ={ "-", ".", "\0" };
-  REQUIRE(out.lexems == lexems);
+  Lox::Lexems expected ={ "-", ".", "\0" };
+  REQUIRE(out.lexems == expected);
 }
 
-TEST_CASE("numbers - not a number: 1..", "[Scanner][Numbers][Not][add]"){
+TEST_CASE("numbers - not a number: 1..", "[Scanner][Numbers]"){
   Lox::Input command = "1..";
   auto out = sut.scan(command);
   CheckNoErrors(out);
-  Lox::Lexems lexems ={ "1", ".", ".", "\0" };
-  REQUIRE(out.lexems == lexems);
+  Lox::Lexems expected ={ "1", ".", ".", "\0" };
+  REQUIRE(out.lexems == expected);
 }
 
 TEST_CASE("Mixed input", "[Scanner][Sanity]"){
   Lox::Input command = "{1}=0//helo !";
   auto out = sut.scan(command);
   CheckNoErrors(out);
-  Lox::Lexems lexems ={ "{", "1", "}", "=", "0", "//helo !", "\0" };
-  REQUIRE(out.lexems == lexems);
+  Lox::Lexems expected ={ "{", "1", "}", "=", "0", "//helo !", "\0" };
+  REQUIRE(out.lexems == expected);
 }
 
-TEST_CASE("Mixed input2", "[Scanner][Sanity][Mix2]"){
+TEST_CASE("Mixed input2", "[Scanner][Sanity]"){
   Lox::Input command = "asdf(){return 1==0;}; //helo !";
   auto out = sut.scan(command);
   CheckNoErrors(out);
-  Lox::Lexems lexems ={ "asdf", "(", ")", "{", "return", "1", "==", "0", ";", "}", ";", "//helo !", "\0" };
-  REQUIRE(out.lexems == lexems);
+  Lox::Lexems expected ={ "asdf", "(", ")", "{", "return", "1", "==", "0", ";", "}", ";", "//helo !", "\0" };
+  REQUIRE(out.lexems == expected);
 }
 
-TEST_CASE("Mixed input2.1", "[Scanner][Sanity][Mix21]"){
+TEST_CASE("Mixed input2.1", "[Scanner][Sanity]"){
   Lox::Input command = "identifier!";
   auto out = sut.scan(command);
   CheckNoErrors(out);
-  Lox::Lexems lexems ={ "identifier", "!", "\0" };
-  REQUIRE(out.lexems == lexems);
+  Lox::Lexems expected ={ "identifier", "!", "\0" };
+  REQUIRE(out.lexems == expected);
 }
 
 TEST_CASE("Mixed input3", "[Scanner][Sanity]"){
   Lox::Input command = "asdf\nddd\n123\nqwe2";
   auto out = sut.scan(command);
   CheckNoErrors(out);
-  Lox::Lexems lexems ={ "asdf", "ddd", "123", "qwe2", "\0" };
-  REQUIRE(out.lexems == lexems);
+  Lox::Lexems expected ={ "asdf", "ddd", "123", "qwe2", "\0" };
+  REQUIRE(out.lexems == expected);
 }
 
 TEST_CASE("Simple empty string", "[Scanner][Empty][String]"){
   Lox::Input command = "\"\"";
   auto out = sut.scan(command);
   CheckNoErrors(out);
-  Lox::Lexems lexems ={ "\"\"", "\0" };
-  REQUIRE(out.lexems == lexems);
+  Lox::Lexems expected ={ "\"\"", "\0" };
+  REQUIRE(out.lexems == expected);
 }
 
 TEST_CASE("Simple nonempty string", "[Scanner][Filled][String]"){
   Lox::Input command = "\"hello\"";
   auto out = sut.scan(command);
   CheckNoErrors(out);
-  Lox::Lexems lexems ={ "\"hello\"", "\0" };
-  REQUIRE(out.lexems == lexems);
+  Lox::Lexems expected ={ "\"hello\"", "\0" };
+  REQUIRE(out.lexems == expected);
 }
 
-TEST_CASE("String with numbers", "[Scanner][String][Numbers][2hello6]"){
+TEST_CASE("String with numbers", "[Scanner][String][Numbers]"){
   Lox::Input command = "2 \"hello\" 6";
   auto out = sut.scan(command);
   CheckNoErrors(out);
-  Lox::Lexems lexems ={ "2", "\"hello\"", "6", "\0" };
-  REQUIRE(out.lexems == lexems);
+  Lox::Lexems expected ={ "2", "\"hello\"", "6", "\0" };
+  REQUIRE(out.lexems == expected);
 }
 
 TEST_CASE("Identifier short", "[Scanner][Identifier]"){
   Lox::Input command = "id";
   auto out = sut.scan(command);
   CheckNoErrors(out);
-  Lox::Lexems lexems ={ "id", "\0" };
-  REQUIRE(out.lexems == lexems);
+  Lox::Lexems expected ={ "id", "\0" };
+  REQUIRE(out.lexems == expected);
 }
 
 TEST_CASE("Identifier long", "[Scanner][Identifier]"){
   Lox::Input command = "commonId";
   auto out = sut.scan(command);
   CheckNoErrors(out);
-  Lox::Lexems lexems ={ "commonId", "\0" };
-  REQUIRE(out.lexems == lexems);
+  Lox::Lexems expected ={ "commonId", "\0" };
+  REQUIRE(out.lexems == expected);
 }
 
 }//namespace Scanner
