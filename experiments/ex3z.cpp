cc7388967231e7006a4d0cee2aa96ef799868be1
pgum <killmepl@gmail.com>
2019-06-30 23:18:42 +0200

    AST fun, memory leak example

--- experiments/ex3z.cpp
+++ experiments/ex3z.cpp
@@ -1,4 +1,4 @@
-//g++ -o ex3 ex3.cpp -Wall -Wextra -Werror -std=c++2a && ./ex3
+//g++ -o ex3z ex3z.cpp -Wall -Wextra -Werror -std=c++2a && ./ex3z
 #include <iostream>
 #include <memory>
 #include <vector>
@@ -15,7 +15,8 @@ using ExprPtr = std::unique_ptr<ExprBase>;
 template<size_t Arity>
 class Expr: public ExprBase {
   public:
-  Expr(Token _operator): value(_operator){}
+  ~Expr() { std::cout<<"Destroy Expr: "<<value<<" children: "<<children.size()<<"\n"; }
+  Expr(Token _operator): value(_operator){ std::cout<<"Create Expr: "<< _operator << "\n";}
   void traverse() override{
     std::cout << "(" << value << ' ';
     for(auto const& child : children) child->traverse();
@@ -27,25 +28,30 @@ class Expr: public ExprBase {
   }
   protected:
   Token value;
-  std::vector<ExprPtr> children; //possible culprit? how to avoid?
+  std::vector<ExprPtr> children;
 };
 
 class Terminal: public Expr<0>{
   public:
-  Terminal(Token val) : Expr(val){}
+  ~Terminal() { std::cout<<"Destroy Terminal: "<<value<<"\n"; }
+  Terminal(Token val) : Expr(val){ std::cout<<"Create Terminal: "<< val << "\n"; }
   void traverse(){ std::cout << value << ' '; }
 };
 
 class Unary: public Expr<1>{
   public:
+  ~Unary() { std::cout<<"Destroy Unary "<<value<<"\n"; }
   Unary(Token _operator, ExprPtr rhs= nullptr): Expr(_operator){
+    std::cout<<"Create Unary: "<< _operator << "\n";
     add(std::move(rhs));
   }
 };
 
 class Binary: public Expr<2>{
   public:
+  ~Binary() { std::cout<<"Destroy Binary "<<value<<"\n"; }
   Binary(Token _operator, ExprPtr lhs= nullptr, ExprPtr rhs= nullptr): Expr(_operator){
+    std::cout<<"Create Binary: "<< _operator << "\n";
     add(std::move(lhs));
     add(std::move(rhs));
   }
@@ -53,14 +59,15 @@ class Binary: public Expr<2>{
 
 int main()
 {
+ 
   auto t_1 = std::make_unique<Terminal>(Token("10"));
   auto t_2 = std::make_unique<Terminal>(Token("2.3"));
   auto t_3 = std::make_unique<Terminal>(Token("-12.3"));
 
   auto lhs = std::make_unique<Binary>(Token("+"), std::move(t_1), std::move(t_2));
   auto rhs = std::make_unique<Unary>(Token("-"), std::move(t_3));
+  //( ( 10 + 2.3 ) - ( - -12.3 ) )
   auto tree = Binary(Token("-"), std::move(lhs), std::move(rhs));
-  //(- (+ 10 2.3 ) (- -12.3 ) )
   tree.traverse();
   std::cout << '\n';
 }
